884ff428832462409e3e3ff9f4e5ee4731e1fdea
EasternCatNZL <brianpwwong@gmail.com>
2018-05-20 23:26:47 +1200

    Project file

---
+++ Zodiac/Assets/Scripts/MapGrid/TileBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehavior : MonoBehaviour {
+
+    struct TileInformation{
+        int xPos; //the x pos of the tile in grid
+        int zPos; //the z pos of the tile in grid
+        int height; //the height of the tile in map
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
