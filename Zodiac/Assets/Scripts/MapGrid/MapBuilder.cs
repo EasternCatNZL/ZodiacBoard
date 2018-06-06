553d4126c69ff01a90ff56a3278f6b8336a9efd9
Brian <brianpwwong@gmail.com>
2018-06-06 16:43:40 +1200

    Started map builder

---
+++ Zodiac/Assets/Scripts/MapGrid/MapBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBuilder : MonoBehaviour {
+
+    [Header("Map to build")]
+    [Tooltip("The map handler object holding the map details")]
+    public MapHandler map;
+
+    [Header("Prefabs for tiles")]
+    [Tooltip("The objects used to represent the tiles")]
+    public GameObject[] tileArray = new GameObject[0];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
