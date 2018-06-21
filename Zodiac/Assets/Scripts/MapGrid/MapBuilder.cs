367f85fabcb9f6a1d76ecd917d1e97a5953006d6
Brian <brianpwwong@gmail.com>
2018-06-21 15:02:43 +1200

    More map building stuff

--- Zodiac/Assets/Scripts/MapGrid/MapBuilder.cs
+++ Zodiac/Assets/Scripts/MapGrid/MapBuilder.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MapBuilder : MonoBehaviour {
 
+    [Header("Tile objects")]
+    [Tooltip("Basic tile")]
+    public GameObject basicTile;
+    [Tooltip("Void tile")]
+    public GameObject voidTile;
+
     [Header("Map to build")]
     [Tooltip("The map handler object holding the map details")]
     public MapHandler map;
@@ -21,4 +27,31 @@ public class MapBuilder : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    //builds out the map into the world
+    private void BuildMap()
+    {
+        //if map not null, reference the board details of map
+        if (map)
+        {
+            List<TileBehavior> board = map.GetBoardDetails();
+            //for all objects in map
+            for (int i = 0; i < board.Count; i++)
+            {
+                //if the current object is not null
+                if (board[i])
+                {
+                    //create a tile based on 
+                }
+            }
+        }
+
+       
+    }
+
+    //builds connections between tiles once map is complete
+    private void BuildConnections()
+    {
+
+    }
 }
